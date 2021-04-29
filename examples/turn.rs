10d4773ea8a207b2eb31e027af6f94c688a8785a
David Himmelstrup <lemmih@gmail.com>
2021-04-29 20:34:36 +0800

    More formatting.

--- examples/turn.rs
+++ examples/turn.rs
@@ -5,9 +5,6 @@ fn main() {
   dbg!(Point([0, 0]).turn(&Point([0, 1]), &Point([2, 2])));
   dbg!(Point([0, 0]).turn(&Point([0, 1]), &Point([-2, 2])));
   dbg!(Point([0, 0]).turn(&Point([0, 0]), &Point([0, 0])));
-  dbg!(Point([0.1, 0.1]).turn(
-    &Point([0.3, 0.3]),
-    &Point([0.9, 0.9])
-  ));
+  dbg!(Point([0.1, 0.1]).turn(&Point([0.3, 0.3]), &Point([0.9, 0.9])));
   // dbg!(raw_arr_turn(&[0.1, 0.1], &[0.3, 0.3], &[0.9, 0.9]));
 }
