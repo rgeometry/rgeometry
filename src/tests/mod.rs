c19421bd2192b34438ccc3e673c25f994cc7652a
David Himmelstrup <lemmih@gmail.com>
2021-05-18 12:32:14 +0800

    Re-enable orientation tests.

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -3,31 +3,31 @@ mod tests {
   use crate::*;
   #[test]
   fn test_turns() {
-    // assert_eq!(
-    //   Point([0, 0]).turn(&Point([1, 1]), &Point([2, 2])),
-    //   Turn::CoLinear
-    // );
-    // assert_eq!(
-    //   Point([0.0, 0.0]).turn(&Point([1.0, 1.0]), &Point([2.0, 2.0])),
-    //   Turn::CoLinear
-    // );
+    assert_eq!(
+      Point::new([0, 0]).turn(&Point::new([1, 1]), &Point::new([2, 2])),
+      Turn::CoLinear
+    );
+    assert_eq!(
+      Point::new([0.0, 0.0]).turn(&Point::new([1.0, 1.0]), &Point::new([2.0, 2.0])),
+      Turn::CoLinear
+    );
 
-    // assert_eq!(
-    //   Point([0, 0]).turn(&Point([0, 1]), &Point([2, 2])),
-    //   Turn::ClockWise
-    // );
-    // assert_eq!(
-    //   Point([0.0, 0.0]).turn(&Point([0.0, 1.0]), &Point([2.0, 2.0])),
-    //   Turn::ClockWise
-    // );
+    assert_eq!(
+      Point::new([0, 0]).turn(&Point::new([0, 1]), &Point::new([2, 2])),
+      Turn::ClockWise
+    );
+    assert_eq!(
+      Point::new([0.0, 0.0]).turn(&Point::new([0.0, 1.0]), &Point::new([2.0, 2.0])),
+      Turn::ClockWise
+    );
 
-    // assert_eq!(
-    //   Point([0, 0]).turn(&Point([0, 1]), &Point([-2, 2])),
-    //   Turn::CounterClockWise
-    // );
-    // assert_eq!(
-    //   Point([0, 0]).turn(&Point([0, 0]), &Point([0, 0])),
-    //   Turn::CoLinear
-    // );
+    assert_eq!(
+      Point::new([0, 0]).turn(&Point::new([0, 1]), &Point::new([-2, 2])),
+      Turn::CounterClockWise
+    );
+    assert_eq!(
+      Point::new([0, 0]).turn(&Point::new([0, 0]), &Point::new([0, 0])),
+      Turn::CoLinear
+    );
   }
 }
