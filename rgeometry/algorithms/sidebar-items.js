8496b30641845f26e729bf251c6b87f511cdb122
Lemmih <lemmih@gmail.com>
2021-05-30 07:31:20 +0000

    Deploying to gh-pages from  @ 77894ab2803d4bf0a18b021c106822cf294a1c26 🚀

--- rgeometry/algorithms/sidebar-items.js
+++ rgeometry/algorithms/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["convex_hull","$O(n log n)$ Convex hull of a set of points."]],"mod":[["convex_hull",""]]});
+initSidebarItems({"fn":[["convex_hull","$O(n \\log n)$ Convex hull of a set of points."]],"mod":[["convex_hull",""]]});
