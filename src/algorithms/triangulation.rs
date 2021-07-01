270a4312fb8cbe0ac2e3c814af92740f47263591
David Himmelstrup <lemmih@gmail.com>
2021-07-01 23:13:37 +0800

    Add convenience functions for triangulation.

--- src/algorithms/triangulation.rs
+++ src/algorithms/triangulation.rs
@@ -1,1 +1,16 @@
+use crate::data::{PointId, Polygon};
+use crate::PolygonScalar;
+
 pub mod earclip;
+
+// FIXME: impl for PolygonConvex, Triangle, etc.
+pub trait Triangulate {
+  fn triangulate(&self) -> Box<dyn Iterator<Item = (PointId, PointId, PointId)> + '_>;
+}
+
+// FIXME: Use hashed triangulation by default.
+impl<T: PolygonScalar> Triangulate for Polygon<T> {
+  fn triangulate(&self) -> Box<dyn Iterator<Item = (PointId, PointId, PointId)> + '_> {
+    Box::new(earclip::earclip(self))
+  }
+}
