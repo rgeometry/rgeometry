325a0372a848cbaa1ed8117c949f157e73ae1e37
David Himmelstrup <lemmih@gmail.com>
2021-07-03 20:18:22 +0800

    wasm server: replace websocket backend with an html server.

--- playground/server/src/compile.rs
+++ playground/server/src/compile.rs
@@ -5,17 +5,44 @@ use std::hash::{Hash, Hasher};
 use std::{fmt, fs, str};
 
 use tokio::process::Command;
+use rocket::*;
 
 const GIT_VERSION: &str = git_version::git_version!();
 
-#[derive(Debug)]
-struct CompileError(String);
+#[derive(Responder, Debug)]
+#[response(status = 200, content_type = "plain")]
+pub struct CompileError(String);
 
 impl CompileError {
   fn new(msg: &str) -> CompileError {
     CompileError(msg.to_string())
   }
 }
+
+// impl From<CompileError> for String {
+//   fn from(error: CompileError) -> String {
+//     error.0
+//   }
+// }
+
+impl From<std::io::Error> for CompileError {
+  fn from(error: std::io::Error) -> CompileError {
+    CompileError(error.to_string())
+  }
+}
+
+impl From<std::str::Utf8Error> for CompileError {
+  fn from(error: std::str::Utf8Error) -> CompileError {
+    CompileError(error.to_string())
+  }
+}
+
+impl From<reqwest::Error> for CompileError {
+  fn from(error: reqwest::Error) -> CompileError {
+    CompileError(error.to_string())
+  }
+}
+
 
 impl fmt::Display for CompileError {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -36,7 +63,7 @@ fn hash_code(code: &str) -> String {
   base64::encode_config(s.finish().to_le_bytes(), base64::URL_SAFE)
 }
 
-pub async fn compile(mut code: String) -> Result<String, Box<dyn Error + Send + Sync>> {
+pub async fn compile(mut code: String) -> Result<std::path::PathBuf, CompileError> {
   code += "\nmod support;\n";
   let proj_dirs = ProjectDirs::from("com", "rgeometry", "rgeometry").unwrap();
   let cache_dir = proj_dirs.cache_dir();
@@ -44,7 +71,7 @@ pub async fn compile(mut code: String) -> Result<String, Box<dyn Error + Send + 
   let html_file = cache_dir.join(&hash).with_extension("html");
   fs::create_dir_all(cache_dir)?;
   if html_file.exists() {
-    return Ok(hash);
+    return Ok(html_file);
   }
 
   fs::write("playground/wasm/src/lib.rs", &code)?;
@@ -67,12 +94,12 @@ pub async fn compile(mut code: String) -> Result<String, Box<dyn Error + Send + 
 
     if status.success() {
       fs::copy("playground/wasm/rgeometry-wasm.html", &html_file)?;
-      Ok(hash)
+      Ok(html_file)
     } else {
-      Err(Box::new(CompileError::new("Internal error: Failed to bundle wasm module.")))
+      Err(CompileError::new("Internal error: Failed to bundle wasm module."))
     }
   } else {
     let stderr = str::from_utf8(&output.stderr)?;
-    Err(Box::new(CompileError::new(stderr)))
+    Err(CompileError::new(stderr))
   }
 }
