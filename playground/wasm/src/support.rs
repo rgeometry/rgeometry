bc79f0f03b4573b4e3a77b6409f59c5b39e8ad82
David Himmelstrup <lemmih@gmail.com>
2021-07-07 08:08:06 +0800

    Set sensible defaults and import common namespaces.

--- playground/wasm/src/support.rs
+++ playground/wasm/src/support.rs
@@ -1,16 +1,7 @@
 #![allow(unused_imports)]
 use gloo_events::EventListener;
-use std::f64;
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
-use web_sys::EventTarget;
 
-use num_bigint::BigInt;
-use num_rational::BigRational;
-use num_traits::cast::ToPrimitive;
-use rand::distributions::Standard;
-use rand::Rng;
-use rgeometry::*;
 use rgeometry_wasm::playground::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -18,14 +9,6 @@ use rgeometry_wasm::playground::*;
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-extern "C" {
-  fn alert(s: &str);
-
-  #[wasm_bindgen(js_namespace = console)]
-  fn log(s: &str);
-}
 
 #[wasm_bindgen(start)]
 pub fn run() {
@@ -47,30 +30,16 @@ pub fn run() {
     redraw()
   });
 
+  // Defaults
+  set_viewport(2.0, 2.0);
+  set_font("24px Menlo");
+  set_text_align("center");
+  set_text_baseline("middle");
+
   redraw();
 }
 
 fn redraw() {
-  let document = web_sys::window().unwrap().document().unwrap();
-  let canvas = document.get_element_by_id("canvas").unwrap();
-  let canvas: web_sys::HtmlCanvasElement = canvas
-    .dyn_into::<web_sys::HtmlCanvasElement>()
-    .map_err(|_| ())
-    .unwrap();
-
-  let context = canvas
-    .get_context("2d")
-    .unwrap()
-    .unwrap()
-    .dyn_into::<web_sys::CanvasRenderingContext2d>()
-    .unwrap();
-
-  context.reset_transform().unwrap();
-
-  context
-    .translate(canvas.width() as f64 / 2., canvas.height() as f64 / 2.)
-    .unwrap();
-
   clear_screen();
 
   super::main();
