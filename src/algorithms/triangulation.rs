c2365dda33b15533cf96b9f0d03563ebc8521ea0
David Himmelstrup <lemmih@gmail.com>
2021-07-07 10:52:21 +0800

    Drop the Triangulate trait.

--- src/algorithms/triangulation.rs
+++ src/algorithms/triangulation.rs
@@ -2,17 +2,3 @@ use crate::data::{PointId, Polygon};
 use crate::PolygonScalar;
 
 pub mod earclip;
-
-// FIXME: impl for PolygonConvex, Triangle, etc.
-pub trait Triangulate {
-  type Iter: Iterator<Item = (PointId, PointId, PointId)>;
-  fn triangulate(self) -> Self::Iter;
-}
-
-// FIXME: Use hashed triangulation by default.
-impl<'a, T: PolygonScalar> Triangulate for &'a Polygon<T> {
-  type Iter = Box<dyn Iterator<Item = (PointId, PointId, PointId)> + 'a>;
-  fn triangulate(self) -> Self::Iter {
-    Box::new(earclip::earclip(self))
-  }
-}
