7bddd9ad6d1286588ebc1510f70bf942ae556e40
obayomy <obayomy@gmail.com>
2021-07-09 14:05:24 +0200

    add files for polygon visibility algorithm

---
+++ src/algorithms/polygon_visibility/naive.rs
@@ -0,0 +1,15 @@
+use crate::algorithms::monotone_polygon::new_monotone_polygon;
+use crate::data::{Point, Polygon};
+use crate::{Error, PolygonScalar};
+
+// pub fn get_visibility_polygon<T>(point: &Point<T, 2>, polygon: &Polygon<T>) -> Result<Polygon<T>,Error>
+// where
+//   T: PolygonScalar,
+// {
+
+// }
+
+#[cfg(test)]
+mod naive_testing {
+  use super::*;
+}
