3de30f832b333b4974c3789cef4f70a76d6efa68
obayomy <obayomy@gmail.com>
2021-07-12 23:04:19 +0200

    Intial test and naive algorithm (test fails)

--- src/algorithms/polygon_visibility/naive.rs
+++ src/algorithms/polygon_visibility/naive.rs
@@ -1,6 +1,12 @@
+use std::iter::FromIterator;
+use std::panic::Location;
+
 use crate::algorithms::monotone_polygon::new_monotone_polygon;
-use crate::data::{Point, Polygon};
-use crate::{Error, PolygonScalar};
+use crate::data::{
+  Direction, EndPoint, HalfLineSoS, Line, LineSegment, LineSegmentView, LineSoS, Point,
+  PointLocation, Polygon,
+};
+use crate::{Intersects, Orientation, PolygonScalar, SoS};
 
 // Note: The description on wikipedia is quite bad.
 // Wikipedia: https://en.wikipedia.org/wiki/Visibility_polygon#Naive_algorithms
@@ -72,14 +78,121 @@ use crate::{Error, PolygonScalar};
 //  |    x    |     |    x    |
 //  \---------/     \---------/
 
-// pub fn get_visibility_polygon<T>(point: &Point<T, 2>, polygon: &Polygon<T>) -> Result<Polygon<T>,Error>
-// where
-//   T: PolygonScalar,
-// {
+pub fn get_visibility_polygon<T>(point: &Point<T, 2>, polygon: &Polygon<T>) -> Option<Polygon<T>>
+where
+  T: PolygonScalar,
+{
+  //check whether the point is in the polygon or not
+  if polygon.locate(point) == PointLocation::Outside {
+    return Option::None;
+  }
 
-// }
+  let polygon_segments: Vec<LineSegment<T, 2>> = polygon
+    .points
+    .windows(2)
+    .map(|pair| {
+      LineSegment::new(
+        EndPoint::Inclusive(pair[0].clone()),
+        EndPoint::Inclusive(pair[1].clone()),
+      )
+    })
+    .collect();
 
+  let mut polygon_points = Vec::new();
+  for vertex in &polygon.points {
+    let right_sos = HalfLineSoS {
+      line: LineSoS::new_through(point.clone(), vertex.clone()),
+      lean: SoS::ClockWise,
+    };
+    let left_sos = HalfLineSoS {
+      line: LineSoS::new_through(point.clone(), vertex.clone()),
+      lean: SoS::CounterClockWise,
+    };
+
+    let mut right_intersections = Vec::new();
+    let mut left_intersections = Vec::new();
+
+    polygon_segments.iter().for_each(|segment| {
+      if right_sos.intersect(segment.as_ref()).is_some() {
+        right_intersections.push(get_intersection(&right_sos, segment));
+      }
+      if left_sos.intersect(segment.as_ref()).is_some() {
+        left_intersections.push(get_intersection(&left_sos, segment));
+      }
+    });
+
+    if let Some(point) = right_intersections
+      .iter()
+      .max_by(|curr, next| point.cmp_distance_to(curr, next))
+    {
+      polygon_points.push(point.clone());
+    };
+    if let Some(point) = left_intersections
+      .iter()
+      .max_by(|curr, next| point.cmp_distance_to(curr, next))
+    {
+      polygon_points.push(point.clone());
+    };
+  }
+  polygon_points.sort_by(|curr, nxt| point.ccw_cmp_around(curr, nxt));
+  polygon_points.dedup();
+  Some(Polygon::new(polygon_points).expect("Polygon Creation failed"))
+}
+
+fn get_intersection<T>(sos_line: &HalfLineSoS<T, 2>, segment: &LineSegment<T, 2>) -> Point<T, 2>
+where
+  T: PolygonScalar,
+{
+  let segment_line = Line {
+    origin: segment.min.inner().clone(),
+    direction: Direction::Through(segment.max.inner().clone()),
+  };
+  let sos_line = Line {
+    origin: sos_line.line.origin.clone(),
+    direction: sos_line.line.direction.clone(),
+  };
+  sos_line
+    .intersection_point(&segment_line)
+    .expect("LinesMustIntersect")
+}
 #[cfg(test)]
 mod naive_testing {
   use super::*;
+
+  //   Input            Output
+  //  /-----\ /----\  /-----\ /----\
+  //  |     | |    |  |     | |    |
+  //  |  x  \-/    |  |  x  \------\
+  //  |            |  |            |
+  //  \------------/  \------------/
+  #[test]
+  fn case_1() {
+    let point = Point::new([2, 3]);
+    let input_polygon = Polygon::new(vec![
+      Point::new([0, 0]),
+      Point::new([10, 0]),
+      Point::new([10, 6]),
+      Point::new([6, 6]),
+      Point::new([6, 3]),
+      Point::new([4, 3]),
+      Point::new([4, 6]),
+      Point::new([0, 6]),
+    ])
+    .unwrap();
+    let out_test_points = vec![
+      Point::new([0, 0]),
+      Point::new([10, 0]),
+      Point::new([10, 3]),
+      Point::new([4, 3]),
+      Point::new([4, 6]),
+      Point::new([0, 6]),
+    ];
+    let out_polygon = get_visibility_polygon(&point, &input_polygon);
+    match out_polygon {
+      Some(polygon) => {
+        assert_eq!(out_test_points.len(), polygon.points.len())
+      }
+      None => panic!(),
+    }
+  }
 }
