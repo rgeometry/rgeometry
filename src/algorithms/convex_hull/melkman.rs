80d6b9f857c36917a90506be36f156fdb1888089
David Himmelstrup <lemmih@gmail.com>
2022-08-11 12:51:22 +0200

    Template.

---
+++ src/algorithms/convex_hull/melkman.rs
@@ -0,0 +1,38 @@
+use crate::data::{Point, Polygon, PolygonConvex};
+use crate::PolygonScalar;
+
+// Wikipedia description of the convex hull problem: https://en.wikipedia.org/wiki/Convex_hull_of_a_simple_polygon
+// Melkman's algorithm: https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.512.9681&rep=rep1&type=pdf
+pub fn convex_hull<T>(_polygon: Polygon<T>) -> PolygonConvex<T>
+where
+  T: PolygonScalar,
+{
+  unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::testing::*;
+
+  use claim::assert_ok;
+
+  use proptest::collection::*;
+  use proptest::prelude::*;
+  use test_strategy::proptest;
+
+  #[proptest]
+  fn does_not_panic(poly: Polygon<i8>) {
+    convex_hull(poly);
+  }
+
+  #[proptest]
+  fn is_valid_convex_polygon(poly: Polygon<i8>) {
+    assert_ok!(convex_hull(poly).validate());
+  }
+
+  //   #[proptest]
+  //   fn is_idempotent(poly: PolygonConvex<i8>) {
+  //     assert_eq!(convex_hull(poly), poly)
+  //   }
+}
