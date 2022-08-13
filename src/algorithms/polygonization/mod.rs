a311e206e44d42c49499a27d3450c52f9073d5b6
David Himmelstrup <lemmih@gmail.com>
2022-08-13 19:23:00 +0200

    Remove dead code.

--- src/algorithms/polygonization/mod.rs
+++ src/algorithms/polygonization/mod.rs
@@ -6,7 +6,3 @@ pub use monotone::new_monotone_polygon;
 pub use star::new_star_polygon;
 pub use two_opt::resolve_self_intersections;
 pub use two_opt::two_opt_moves;
-
-// pub use monotone;
-// pub use star;
-// pub use two_opt;
