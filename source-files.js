9d2a2cc8652066c254a4b10295ca6c3d3d94c2fa
lemmih <lemmih@gmail.com>
2022-10-11 09:12:43 +0000

    Deploying to gh-pages from  @ cc8f2676ff7a9bc877235654224bcdc83fc7d372 🚀

--- source-files.js
+++ source-files.js
@@ -1,21 +1,22 @@
-var sourcesIndex = {};
-sourcesIndex["array_init"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["cfg_if"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["claim"] = {"name":"","files":["assert_err.rs","assert_ge.rs","assert_gt.rs","assert_le.rs","assert_lt.rs","assert_matches.rs","assert_none.rs","assert_ok.rs","assert_ok_eq.rs","assert_pending.rs","assert_ready.rs","assert_ready_eq.rs","assert_ready_err.rs","assert_ready_ok.rs","assert_some.rs","assert_some_eq.rs","lib.rs"]};
-sourcesIndex["geometry_predicates"] = {"name":"","files":["lib.rs","predicates.rs"]};
-sourcesIndex["getrandom"] = {"name":"","files":["error.rs","error_impls.rs","lib.rs","linux_android.rs","use_file.rs","util.rs","util_libc.rs"]};
-sourcesIndex["libc"] = {"name":"","dirs":[{"name":"unix","dirs":[{"name":"linux_like","dirs":[{"name":"linux","dirs":[{"name":"arch","dirs":[{"name":"generic","files":["mod.rs"]}],"files":["mod.rs"]},{"name":"gnu","dirs":[{"name":"b64","dirs":[{"name":"x86_64","files":["align.rs","mod.rs","not_x32.rs"]}],"files":["mod.rs"]}],"files":["align.rs","mod.rs"]}],"files":["align.rs","mod.rs","non_exhaustive.rs"]}],"files":["mod.rs"]}],"files":["align.rs","mod.rs"]}],"files":["fixed_width_ints.rs","lib.rs","macros.rs"]};
-sourcesIndex["num"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["num_bigint"] = {"name":"","dirs":[{"name":"bigint","files":["addition.rs","bits.rs","convert.rs","division.rs","multiplication.rs","power.rs","shift.rs","subtraction.rs"]},{"name":"biguint","files":["addition.rs","bits.rs","convert.rs","division.rs","iter.rs","monty.rs","multiplication.rs","power.rs","shift.rs","subtraction.rs"]}],"files":["bigint.rs","biguint.rs","lib.rs","macros.rs"]};
-sourcesIndex["num_complex"] = {"name":"","files":["cast.rs","complex_float.rs","lib.rs","pow.rs"]};
-sourcesIndex["num_integer"] = {"name":"","files":["average.rs","lib.rs","roots.rs"]};
-sourcesIndex["num_iter"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["num_rational"] = {"name":"","files":["lib.rs","pow.rs"]};
-sourcesIndex["num_traits"] = {"name":"","dirs":[{"name":"ops","files":["checked.rs","euclid.rs","inv.rs","mod.rs","mul_add.rs","overflowing.rs","saturating.rs","wrapping.rs"]}],"files":["bounds.rs","cast.rs","float.rs","identities.rs","int.rs","lib.rs","macros.rs","pow.rs","real.rs","sign.rs"]};
-sourcesIndex["ordered_float"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["ppv_lite86"] = {"name":"","dirs":[{"name":"x86_64","files":["mod.rs","sse2.rs"]}],"files":["lib.rs","soft.rs","types.rs"]};
-sourcesIndex["rand"] = {"name":"","dirs":[{"name":"distributions","files":["bernoulli.rs","distribution.rs","float.rs","integer.rs","mod.rs","other.rs","slice.rs","uniform.rs","utils.rs","weighted.rs","weighted_index.rs"]},{"name":"rngs","dirs":[{"name":"adapter","files":["mod.rs","read.rs","reseeding.rs"]}],"files":["mock.rs","mod.rs","small.rs","std.rs","thread.rs","xoshiro256plusplus.rs"]},{"name":"seq","files":["index.rs","mod.rs"]}],"files":["lib.rs","prelude.rs","rng.rs"]};
-sourcesIndex["rand_chacha"] = {"name":"","files":["chacha.rs","guts.rs","lib.rs"]};
-sourcesIndex["rand_core"] = {"name":"","files":["block.rs","error.rs","impls.rs","le.rs","lib.rs","os.rs"]};
-sourcesIndex["rgeometry"] = {"name":"","dirs":[{"name":"algorithms","dirs":[{"name":"convex_hull","files":["graham_scan.rs","melkman.rs","mod.rs"]},{"name":"intersection","files":["naive.rs"]},{"name":"polygonization","files":["mod.rs","monotone.rs","star.rs","two_opt.rs"]},{"name":"triangulation","files":["earclip.rs"]},{"name":"visibility","files":["mod.rs","naive.rs"]}],"files":["intersection.rs","triangulation.rs","zhash.rs"]},{"name":"data","dirs":[{"name":"point","files":["add.rs","sub.rs"]},{"name":"polygon","files":["convex.rs","iter.rs"]},{"name":"vector","files":["add.rs","div.rs","mul.rs","sub.rs"]}],"files":["directed_edge.rs","intersection_set.rs","line.rs","line_segment.rs","point.rs","polygon.rs","triangle.rs","vector.rs"]}],"files":["algorithms.rs","data.rs","intersection.rs","lib.rs","matrix.rs","orientation.rs","transformation.rs","utils.rs"]};
+var sourcesIndex = JSON.parse('{\
+"array_init":["",[],["lib.rs"]],\
+"cfg_if":["",[],["lib.rs"]],\
+"claims":["",[],["assert_err.rs","assert_err_eq.rs","assert_ge.rs","assert_gt.rs","assert_le.rs","assert_lt.rs","assert_matches.rs","assert_none.rs","assert_ok.rs","assert_ok_eq.rs","assert_pending.rs","assert_ready.rs","assert_ready_eq.rs","assert_ready_err.rs","assert_ready_ok.rs","assert_some.rs","assert_some_eq.rs","lib.rs"]],\
+"geometry_predicates":["",[],["lib.rs","predicates.rs"]],\
+"getrandom":["",[],["error.rs","error_impls.rs","lib.rs","linux_android.rs","use_file.rs","util.rs","util_libc.rs"]],\
+"libc":["",[["unix",[["linux_like",[["linux",[["arch",[["generic",[],["mod.rs"]]],["mod.rs"]],["gnu",[["b64",[["x86_64",[],["align.rs","mod.rs","not_x32.rs"]]],["mod.rs"]]],["align.rs","mod.rs"]]],["align.rs","mod.rs","non_exhaustive.rs"]]],["mod.rs"]]],["align.rs","mod.rs"]]],["fixed_width_ints.rs","lib.rs","macros.rs"]],\
+"num":["",[],["lib.rs"]],\
+"num_bigint":["",[["bigint",[],["addition.rs","bits.rs","convert.rs","division.rs","multiplication.rs","power.rs","shift.rs","subtraction.rs"]],["biguint",[],["addition.rs","bits.rs","convert.rs","division.rs","iter.rs","monty.rs","multiplication.rs","power.rs","shift.rs","subtraction.rs"]]],["bigint.rs","biguint.rs","lib.rs","macros.rs"]],\
+"num_complex":["",[],["cast.rs","complex_float.rs","lib.rs","pow.rs"]],\
+"num_integer":["",[],["average.rs","lib.rs","roots.rs"]],\
+"num_iter":["",[],["lib.rs"]],\
+"num_rational":["",[],["lib.rs","pow.rs"]],\
+"num_traits":["",[["ops",[],["checked.rs","euclid.rs","inv.rs","mod.rs","mul_add.rs","overflowing.rs","saturating.rs","wrapping.rs"]]],["bounds.rs","cast.rs","float.rs","identities.rs","int.rs","lib.rs","macros.rs","pow.rs","real.rs","sign.rs"]],\
+"ordered_float":["",[],["lib.rs"]],\
+"ppv_lite86":["",[["x86_64",[],["mod.rs","sse2.rs"]]],["lib.rs","soft.rs","types.rs"]],\
+"rand":["",[["distributions",[],["bernoulli.rs","distribution.rs","float.rs","integer.rs","mod.rs","other.rs","slice.rs","uniform.rs","utils.rs","weighted.rs","weighted_index.rs"]],["rngs",[["adapter",[],["mod.rs","read.rs","reseeding.rs"]]],["mock.rs","mod.rs","small.rs","std.rs","thread.rs","xoshiro256plusplus.rs"]],["seq",[],["index.rs","mod.rs"]]],["lib.rs","prelude.rs","rng.rs"]],\
+"rand_chacha":["",[],["chacha.rs","guts.rs","lib.rs"]],\
+"rand_core":["",[],["block.rs","error.rs","impls.rs","le.rs","lib.rs","os.rs"]],\
+"rgeometry":["",[["algorithms",[["convex_hull",[],["graham_scan.rs","melkman.rs","mod.rs"]],["intersection",[],["naive.rs"]],["polygonization",[],["mod.rs","monotone.rs","star.rs","two_opt.rs"]],["triangulation",[],["earclip.rs"]],["visibility",[],["mod.rs","naive.rs"]]],["intersection.rs","triangulation.rs","zhash.rs"]],["data",[["point",[],["add.rs","sub.rs"]],["polygon",[],["convex.rs","iter.rs"]],["vector",[],["add.rs","div.rs","mul.rs","sub.rs"]]],["directed_edge.rs","intersection_set.rs","line.rs","line_segment.rs","point.rs","polygon.rs","triangle.rs","vector.rs"]]],["algorithms.rs","data.rs","intersection.rs","lib.rs","matrix.rs","orientation.rs","transformation.rs","utils.rs"]]\
+}');
 createSourceSidebar();
