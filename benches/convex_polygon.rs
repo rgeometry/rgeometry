ade66ed5820ed6fd606daea6533117484d7d96de
David Himmelstrup <lemmih@gmail.com>
2025-01-03 12:06:22 +0100

    feat: trim dependencies to support no-std environments (#113)

--- benches/convex_polygon.rs
+++ benches/convex_polygon.rs
@@ -1,8 +1,10 @@
 use criterion::{criterion_group, criterion_main, Criterion};
+use rand::rngs::SmallRng;
+use rand::SeedableRng;
 use rgeometry::data::PolygonConvex;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-  let mut rng = rand::thread_rng();
+  let mut rng = SmallRng::seed_from_u64(0xdeadbeef);
   c.bench_function("PolygonConvex::random(20)", |b| {
     b.iter(|| PolygonConvex::<i64>::random(20, &mut rng))
   });
