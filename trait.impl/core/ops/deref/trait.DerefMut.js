b9b96a96a665991dd6a39cf954ff76eee76a9c4a
lemmih <lemmih@gmail.com>
2025-08-03 06:40:35 +0000

    Deploying to gh-pages from @ rgeometry/rgeometry@1abcd82ce3ed09766b3e121cf0f7ac0feb492a3e 🚀

--- trait.impl/core/ops/deref/trait.DerefMut.js
+++ trait.impl/core/ops/deref/trait.DerefMut.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["ordered_float",[["impl&lt;T: <a class=\"trait\" href=\"ordered_float/trait.FloatCore.html\" title=\"trait ordered_float::FloatCore\">FloatCore</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"ordered_float/struct.OrderedFloat.html\" title=\"struct ordered_float::OrderedFloat\">OrderedFloat</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["ordered_float",[["impl&lt;T: <a class=\"trait\" href=\"ordered_float/trait.FloatCore.html\" title=\"trait ordered_float::FloatCore\">FloatCore</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"ordered_float/struct.OrderedFloat.html\" title=\"struct ordered_float::OrderedFloat\">OrderedFloat</a>&lt;T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
