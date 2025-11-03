b883f6a0e6ec1875ed4da1e6a8be87c4202df68f
lemmih <lemmih@users.noreply.github.com>
2025-11-03 11:31:22 +0000

    Deploy preview for PR 154 🛫

---
+++ pr-preview/pr-154/share/doc/trait.impl/core/iter/traits/exact_size/trait.ExactSizeIterator.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rgeometry",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/iter/traits/exact_size/trait.ExactSizeIterator.html\" title=\"trait core::iter::traits::exact_size::ExactSizeIterator\">ExactSizeIterator</a> for <a class=\"struct\" href=\"rgeometry/data/polygon/struct.CursorIter.html\" title=\"struct rgeometry::data::polygon::CursorIter\">CursorIter</a>&lt;'_, T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[400]}
