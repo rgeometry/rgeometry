49bcb31a48d2eb3df4fc1dd6f08ce922a22502d2
lemmih <lemmih@users.noreply.github.com>
2025-11-05 09:41:15 +0000

    Remove preview for PR 145 🛬

--- pr-preview/pr-145/share/doc/trait.impl/core/ops/index/trait.IndexMut.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["rgeometry",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/ops/index/trait.IndexMut.html\" title=\"trait core::ops::index::IndexMut\">IndexMut</a>&lt;<a class=\"struct\" href=\"rgeometry/data/struct.IndexEdge.html\" title=\"struct rgeometry::data::IndexEdge\">IndexEdge</a>&gt; for <a class=\"struct\" href=\"rgeometry/data/struct.IndexIntersectionSet.html\" title=\"struct rgeometry::data::IndexIntersectionSet\">IndexIntersectionSet</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[468]}
