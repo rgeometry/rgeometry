88d714a6ed207cb042f5bd2a7a8099863d6555c5
lemmih <lemmih@users.noreply.github.com>
2026-01-05 22:06:21 +0000

    Deploy preview for PR 215 🛫

---
+++ pr-preview/pr-215/share/doc/trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rgeometry",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"rgeometry/enum.Error.html\" title=\"enum rgeometry::Error\">Error</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[259]}
