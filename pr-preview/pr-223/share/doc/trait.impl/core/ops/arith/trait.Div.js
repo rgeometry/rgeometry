2393fc54ccb6a1dabf59ffbfeaf50f90850769e6
lemmih <lemmih@users.noreply.github.com>
2026-03-15 02:52:39 +0000

    Deploy preview for PR 223 🛫

---
+++ pr-preview/pr-223/share/doc/trait.impl/core/ops/arith/trait.Div.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rgeometry",[["impl&lt;T, const N: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.91.0/std/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/ops/arith/trait.Div.html\" title=\"trait core::ops::arith::Div\">Div</a>&lt;T&gt; for <a class=\"struct\" href=\"rgeometry/data/struct.Vector.html\" title=\"struct rgeometry::data::Vector\">Vector</a>&lt;T, N&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://docs.rs/num-traits/0.2/num_traits/trait.NumOps.html\" title=\"trait num_traits::NumOps\">NumOps</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a>,</div>"],["impl&lt;T, const N: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.91.0/std/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/ops/arith/trait.Div.html\" title=\"trait core::ops::arith::Div\">Div</a>&lt;T&gt; for <a class=\"struct\" href=\"rgeometry/data/struct.VectorView.html\" title=\"struct rgeometry::data::VectorView\">VectorView</a>&lt;'_, T, N&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://docs.rs/num-traits/0.2/num_traits/trait.NumOps.html\" title=\"trait num_traits::NumOps\">NumOps</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a>,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1463]}
