1f0a2f7c588ad3516a839d7f4b60386427ec3256
lemmih <lemmih@users.noreply.github.com>
2026-04-15 02:57:22 +0000

    Deploy preview for PR 227 🛫

---
+++ pr-preview/pr-227/share/doc/trait.impl/core/convert/trait.TryFrom.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rgeometry",[["impl&lt;const N: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.91.0/std/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"struct\" href=\"rgeometry/data/struct.Vector.html\" title=\"struct rgeometry::data::Vector\">Vector</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.91.0/std/primitive.f64.html\">f64</a>, N&gt;&gt; for <a class=\"struct\" href=\"rgeometry/data/struct.Vector.html\" title=\"struct rgeometry::data::Vector\">Vector</a>&lt;<a class=\"type\" href=\"https://docs.rs/num-rational/0.4/num_rational/type.BigRational.html\" title=\"type num_rational::BigRational\">BigRational</a>, N&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[793]}
